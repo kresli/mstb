d1d8577ef04fdf35833704b6933420fb98c2e34c
Eduard Jacko <jacko.eduard@gmail.com>
2019-07-23 01:05:01 +0100

    fixed rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 import builtins from "rollup-plugin-node-builtins";
 import json from "rollup-plugin-json";
+import replace from "rollup-plugin-replace";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
